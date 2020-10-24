33661a75d99fb3d19c83c77516eb7acb485f55d6
AnaSPires <42739437+AnaSPires@users.noreply.github.com>
2020-10-23 23:06:05 -0300

    envio de comentários
    já é possível mandar comentários e observá-los no post

--- base/index.js
+++ base/index.js
@@ -1,3 +1,34 @@
+var post_atual = '';
+var user_username =  '';
+
+function postComments(comments){
+  var html = '';
+        var altura = 0;
+        const data_atual = new Date().getHours();
+        var horas_comment;
+
+        for(var i = 0; i<comments.length; i++)
+        {
+          const comment = comments[i];
+          horas_comment = data_atual - new Date(comment.created_at).getHours();
+
+        html +='<div class="comment" id="comment">'+
+        '<div class="comment__info" "><b>'+
+        comment.user.username+'</b>'+' '+comment.message+'</div>'+
+        '<div class="comment__avatar" ">'+
+          '<img src="'+comment.user.avatar+'" class="img_comment">'+
+        '</div>'+'<div class="comment__time"">'+
+          horas_comment+'h'+
+      '</div>'+
+      '</div>';
+        
+      
+        //altura = document.getElementById("comment")
+        }
+
+        return html;
+}
+
 (function(apiUrl) {
   function getMe() {
     return fetch(apiUrl + "/me")
@@ -7,6 +38,8 @@
       .then(function(user) {
         const $username = document.getElementById("current-user-username");
         const $avatar = document.getElementById("current-user-avatar");
+
+        user_username = user.username;
         
         $username.innerHTML = user.username;
 
@@ -16,12 +49,17 @@
       });
     }
 
+
       function getPost() {
         return fetch(apiUrl + "/post")
           .then(function(response) {
             return response.json();
           })
           .then(function(post) {
+
+            post_atual = post;
+            //console.log(uuid);
+            
             const $photo = document.getElementById("photo");
             const $avatar = document.getElementById("author-avatar");
             const $username = document.getElementById("author-username");
@@ -41,31 +79,7 @@
              const datac= new Date(post.created_at); 
              $data.innerHTML = datac.getDate() + " de " + month[datac.getMonth()] ;
             
-             var html = '';
-             var altura = 0;
-             const data_atual = new Date().getHours();
-             var horas_comment;
-
-             for(var i = 0; i<comments.length; i++)
-             {
-               const comment = comments[i];
-               horas_comment = data_atual - new Date(comment.created_at).getHours();
-
-              html +='<div class="comment" id="comment">'+
-              '<div class="comment__info" "><b>'+
-              comment.user.username+'</b>'+' '+comment.message+'</div>'+
-              '<div class="comment__avatar" ">'+
-                '<img src="'+comment.user.avatar+'" class="img_comment">'+
-              '</div>'+'<div class="comment__time"">'+
-                horas_comment+'h'+
-            '</div>'+
-            '</div>';
-              
-            
-              //altura = document.getElementById("comment")
-             }
-
-             $comments.innerHTML = html;
+             $comments.innerHTML = postComments(post.comments);
              
 
             const user = post.user;
@@ -84,44 +98,39 @@
           });
         }
 
-        function pegaTamanho(){
-          //pega a resolução
-          var w = screen.availWidth;
-          var h = screen.availHeight;
-          //resolução
-          //define o sistema operacional
-          var OSName="Unknown OS";
-          if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-          if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
-          if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
-          if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
-          //sistema operacional
-
-          //navegadores
-          // mozilla firefox
-          if ($.browser.mozilla == true) {
-            console.log('sistema operacional: '+ OSName +'\n'+ w +'x'+ h +'\n'+'  Firefox / Versão:' + $.browser.version);
-            // internet explorer
-          } else if($.browser.msie == true) {
-            console.log('sistema operacional: '+ OSName +'\n'+ w +'x'+ h +'\n'+'  Internet Explorer / Versão:' + $.browser.version);
-          // webkit
-          } else if($.browser.webkit == true) {
-            console.log('sistema operacional: '+ OSName +'\n'+ w +'x'+ h +'\n'+'  Navegador baseado em WebKit / Versão:' + $.browser.version);
-          // outros navegadores
-          } else {
-            console.log('sistema operacional: '+ OSName + 'Outros nevegadores');
-          }
-        }
   
 
   function initialize() {
     getMe();
     getPost();
-    pegaTamanho();
   }
 
   initialize();
 })("https://taggram.herokuapp.com");
+
+function Enviar() {
+  console.log("cheguei");
+  var message = document.getElementById("new_comment").value;
+
+  fetch(("https://taggram.herokuapp.com" + "/posts/"+post_atual.uuid+"/comments"), {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({"username":user_username, "message":message})
+  })
+  .then(response => response.json())
+  .then(function(data) { 
+  console.log('data is', data);
+  document.getElementById("comment-list").innerHTML = postComments(data);
+  document.getElementById("qtd-comentarios").innerHTML = data.length + " comentários";
+  document.getElementById("new_comment").value = "";
+})
+  .catch(error => console.log('error is', error));
+    
+}
+  
+
 
 
 //navagadores
