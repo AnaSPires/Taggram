e25dfa6f554c18539c5d41ea85e7a84c3ee92df2
AnaSPires <42739437+AnaSPires@users.noreply.github.com>
2020-10-26 22:27:52 -0300

    finalização
    tratamento de erros e logo tagview

--- base/index.js
+++ base/index.js
@@ -1,32 +1,38 @@
+// guarda o post para quando for enviar uma mensagem
 var post_atual = '';
+
+// guarda o username do usuário para quando for enviar uma mensagem
 var user_username =  '';
 
+// função que exibe todos os comentários
 function postComments(comments){
   var html = '';
         var altura = 0;
         const data_atual = new Date().getHours();
         var horas_comment;
+        var cod_avatar;
 
         for(var i = 0; i<comments.length; i++)
         {
           const comment = comments[i];
           horas_comment = data_atual - new Date(comment.created_at).getHours();
-
-        html +='<div class="comment" id="comment">'+
-        '<div class="comment__info" "><b>'+
-        comment.user.username+'</b>'+' '+comment.message+'</div>'+
-        '<div class="comment__avatar" ">'+
-          '<img src="'+comment.user.avatar+'" class="img_comment">'+
-        '</div>'+'<div class="comment__time"">'+
-          horas_comment+'h'+
-      '</div>'+
-      '</div>';
+          if(comment.user.avatar)
+          cod_avatar = '<img src="'+comment.user.avatar+'" class="img_comment">';
+          else
+          cod_avatar='';
         
-      
-        //altura = document.getElementById("comment")
+          html +='<div class="comment" id="comment">'+
+          '<div class="comment__info" "><b>'+
+          comment.user.username+'</b>'+' '+comment.message+'</div>'+
+          '<div class="comment__avatar" ">'+cod_avatar+
+          '</div>'+'<div class="comment__time"">'+
+            horas_comment+'h'+
+        '</div>'+
+        '</div>';
         }
-
+        // retorna o código html pronto com todos os comentários
         return html;
+
 }
 
 (function(apiUrl) {
@@ -36,6 +42,10 @@ function postComments(comments){
         return response.json();
       })
       .then(function(user) {
+        if(data.type == "user_not_found")
+        alert("Fique tranquilo = ) O usuário não foi encontrado, apenas recarregue a página");
+        
+        else{
         const $username = document.getElementById("current-user-username");
         const $avatar = document.getElementById("current-user-avatar");
 
@@ -46,19 +56,22 @@ function postComments(comments){
         if (user.avatar) {
           $avatar.style.backgroundImage = "url('" + user.avatar + "')";
         }
+      }
       });
     }
 
-
+      // função para pegar o post pela api e exibí-la
       function getPost() {
         return fetch(apiUrl + "/post")
           .then(function(response) {
             return response.json();
           })
           .then(function(post) {
-
+            if(data.type == "post_not_found")
+            alert("Fique tranquilo = ) O post não foi encontrado, apenas recarregue a página");
+            
+            else{
             post_atual = post;
-            //console.log(uuid);
             
             const $photo = document.getElementById("photo");
             const $avatar = document.getElementById("author-avatar");
@@ -67,21 +80,24 @@ function postComments(comments){
             const $qtd_comentarios = document.getElementById("qtd-comentarios");
             const $data = document.getElementById("data");
             const $comments = document.getElementById("comment-list");
-
+            
             const location = post.location;
             $info.innerHTML = location.city+", "+location.country;
 
             const comments = post.comments;
             $qtd_comentarios.innerHTML = comments.length + " comentários";
+            
+            // vetor que serve para exíbir na tela pelo nome, já que na api 
+            //só há o número do mês, lembrando que o vetor começa na posição 0
 
-            var month = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+            var month = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", 
+            "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
   
              const datac= new Date(post.created_at); 
-             $data.innerHTML = datac.getDate() + " de " + month[datac.getMonth()] ;
+             $data.innerHTML = datac.getDate() + " de " + month[datac.getMonth() - 1] ;
             
              $comments.innerHTML = postComments(post.comments);
              
-
             const user = post.user;
             $username.innerHTML = user.username;
            
@@ -93,7 +109,7 @@ function postComments(comments){
             if (user.avatar) {
               $avatar.style.backgroundImage = "url('" + post.photo + "')";
             }
-
+          }
             
           });
         }
@@ -108,8 +124,9 @@ function postComments(comments){
   initialize();
 })("https://taggram.herokuapp.com");
 
+// função chamada quando o botão é clicado, adiciona o comentário na api 
+// e exibe os comentários novamente
 function Enviar() {
-  console.log("cheguei");
   var message = document.getElementById("new_comment").value;
 
   fetch(("https://taggram.herokuapp.com" + "/posts/"+post_atual.uuid+"/comments"), {
@@ -121,12 +138,19 @@ function Enviar() {
   })
   .then(response => response.json())
   .then(function(data) { 
-  console.log('data is', data);
-  document.getElementById("comment-list").innerHTML = postComments(data);
-  document.getElementById("qtd-comentarios").innerHTML = data.length + " comentários";
-  document.getElementById("new_comment").value = "";
+  if(data.type == "random_error")
+  alert("Fique tranquilo = ) Você caiu na probabilicade de 33%, apenas recarregue a página");
+  
+  else{
+    document.getElementById("comment-list").innerHTML = postComments(data);
+    document.getElementById("qtd-comentarios").innerHTML = data.length + " comentários";
+    document.getElementById("new_comment").value = "";
+  }
 })
-  .catch(error => console.log('error is', error));
+.catch(function(error){
+  console.log(error);
+})
+  
     
 }
   
